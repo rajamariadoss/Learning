882d18af06cedd4b63bef2f670c4aa25b0437609
Raja Mariadoss <raja.mariadoss@gmail.com>
2021-06-19 13:19:16 -0400

    Add more logic to handle board click

--- react-tic-tac-toe/src/components/Boards.js
+++ react-tic-tac-toe/src/components/Boards.js
@@ -1,58 +1,109 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import './Boards.css';
 
 const Boards = (props) => {
-    // let currentPlayerId = props.currentPlayerId;
+    const [p1Selections,setP1Selections] = useState(['']);
+    const [p2Selections,setP2Selections] = useState(['']);
+
+    const winningCombinations = [
+        ['b1','b2','b3'],['b4','b5','b6'],['b7','b8','b9'],
+        ['b1','b4','b7'],['b2','b5','b8'],['b3','b6','b9'],
+        ['b1','b5','b9'], ['b3','b5','b7']
+    ];
+
     const boardClickHandler = (event) => {
-        console.log(event);
-        if (props.currentPlayerId === 'p1') {
-            props.nextPlayer(props.players[1]);
-            event.target.innerHTML = props.players[0].symbol;
-        } else {
-            props.nextPlayer(props.players[0]);
-            event.target.innerHTML = props.players[1].symbol;
+
+        const checkWinner = (playerSelections) => {
+
+            let winner = false;
+    
+            winningCombinations.forEach(function(wc, index, array) {
+                if  (wc.every(r => playerSelections.includes(r))) {
+                    //console.log(wc, index);
+                    winner = true;
+                }
+              });
+            return winner;
+        };
+
+        let symbol;
+        // console.log ("event target id::" + event.target.id);
+        // console.log ("player 1 symbol::" + props.players[0].symbol);
+        // console.log ("player 2 symbol::" + props.players[1].symbol);
+        // console.log (event.target.innerHTML !== props.players[0].symbol);
+        // console.log (event.target.innerHTML !== props.players[1].symbol);
+        if (event.target.innerHTML !== props.players[0].symbol && event.target.innerHTML !== props.players[1].symbol) {
+            // console.log("Click accepted!");
+            if (props.currentPlayerId === 'p1') {
+                    props.nextPlayer(props.players[1]);
+                    symbol = props.players[0].symbol;
+                    setP1Selections(oldSelections => [...oldSelections, event.target.id]);
+                    if(checkWinner(p1Selections)){
+                        console.log ('player1 wins');
+                    }
+            } else {
+                props.nextPlayer(props.players[0]);
+                symbol = props.players[1].symbol;
+                setP2Selections(oldSelections => [...oldSelections, event.target.id]);
+                if(checkWinner(p2Selections)){
+                    console.log ('player2 wins');
+                }
+            }
+            event.target.innerHTML = symbol;
         }
     };
 
     return (
+        <div>
+        <table>
+            <tbody>
+            <tr>
+                <td>Player 1 Selections:: {p1Selections} </td>
+            </tr>
+            <tr>
+                <td>Player 2 Selections:: {p2Selections}</td>
+            </tr>
+            </tbody>
+        </table>
         <table border='1' width='300'>
             <tbody>
               <tr>
                 <td>
-                    <label onClick={boardClickHandler}>1</label>
+                    <label id="b1" onClick={boardClickHandler}>1</label>
                 </td>
                 <td>
-                    <label onClick={boardClickHandler}>2</label>
+                    <label id="b2" onClick={boardClickHandler}>2</label>
                 </td>
                 <td>
-                    <label onClick={boardClickHandler}>3</label>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                    <label onClick={boardClickHandler}>4</label>
-                </td>
-                <td>
-                    <label onClick={boardClickHandler}>5</label>
-                </td>
-                <td>
-                    <label onClick={boardClickHandler}>6</label>
+                    <label id="b3" onClick={boardClickHandler}>3</label>
                 </td>
               </tr>
               <tr>
                 <td>
-                    <label onClick={boardClickHandler}>7</label>
+                    <label id="b4" onClick={boardClickHandler}>4</label>
                 </td>
                 <td>
-                    <label onClick={boardClickHandler}>8</label>
+                    <label id="b5" onClick={boardClickHandler}>5</label>
                 </td>
                 <td>
-                    <label onClick={boardClickHandler}>9</label>
+                    <label id="b6" onClick={boardClickHandler}>6</label>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                    <label id="b7" onClick={boardClickHandler}>7</label>
+                </td>
+                <td>
+                    <label id="b8" onClick={boardClickHandler}>8</label>
+                </td>
+                <td>
+                    <label id="b9" onClick={boardClickHandler}>9</label>
                 </td>
               </tr>
           </tbody>
         </table>
+        </div>
     )
 };
 
