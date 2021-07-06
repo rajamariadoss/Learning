be96e2fbf24fd7fa809d36b0f6ba5f32ad76c51e
Raja Mariadoss <raja.mariadoss@gmail.com>
2021-07-06 14:39:51 -0400

    using Wrapper, fragments and portals

--- react-practice-project/src/components/Users/AddUser.js
+++ react-practice-project/src/components/Users/AddUser.js
@@ -3,6 +3,7 @@ import React, {useState} from 'react';
 import classes from './AddUser.module.css';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
+import Wrapper from '../Helper/Wrapper';
 import ErrorModal from '../UI/ErrorModal';
 
 const AddUser = (props) => {
@@ -45,7 +46,7 @@ const AddUser = (props) => {
     }
 
     return (
-        <div>
+        <Wrapper>
             {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler} />}
             <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
@@ -56,7 +57,7 @@ const AddUser = (props) => {
                 <Button type="submit">Add User</Button>
             </form>
         </Card>
-        </div>
+        </Wrapper>
     );
 };
 
